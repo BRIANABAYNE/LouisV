5beb397217fb837e637aa65deb75a9642500ea75
Briana Bayne <brianabayne@brianas-imac.lan>
2023-07-04 11:36:56 -0600

    Completed ListView Setup.

---
+++ LouisV1/LouisV1/Model/UI/BagTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BagTableViewCell.swift
+//  LouisV1
+//
+//  Created by Briana Bayne on 7/4/23.
+//
+
+import UIKit
+
+class BagTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
