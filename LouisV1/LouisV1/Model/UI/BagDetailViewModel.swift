a96c22192d41fec04913e4fa3306093f23a81afc
Briana Bayne <brianabayne@brianas-imac.lan>
2023-07-05 16:13:43 -0600

    Bugs

--- LouisV1/LouisV1/Model/UI/BagDetailViewModel.swift
+++ LouisV1/LouisV1/Model/UI/BagDetailViewModel.swift
@@ -10,8 +10,8 @@ import Foundation
 import FirebaseFirestoreSwift
 import FirebaseStorage
 
-protocol BagDetailViewModelDelegate: BagDetailViewController {
-    func imageLoadedSuccessfully() 
+protocol BagDetailViewModelDelegate: AnyObject {
+    func imageLoadedSuccessfully()
 }
 
 
@@ -19,7 +19,8 @@ class BagDetailViewModel {
     
     var bag: Bag?
     var image: UIImage?
-    weak var delegate: BagListViewModelDelegate?
+    weak var delegate: BagDetailViewModelDelegate?
+
     
     init(bag: Bag?, injectedDelegate: BagDetailViewModelDelegate) {
         self.bag = bag
@@ -40,59 +41,43 @@ class BagDetailViewModel {
             case .failure(let failure):
                 print(failure)
             }
-       
-    }
-        
-    
-    // Method singnature
-        func save(parameterBagName: Bag, completion: @escaping (Result<String, FirebaseError>) -> Void) {
-            let ref = Firestore.firestore()
-            do {
-                let documentRef = try ref.collection(Constatns.Bags.bagsCollectionPath).addDocument(from: parameterBagName, completion: { _ in
-                })
-                
-                completion(.success(documentRef.documentID))
-            } catch {
-                print("Oh Shit. Something went wrong", error.localizedDescription)
-                return
-            }
-        } // end of the save
-        
-        func fetchImage(with id: String) {
-            guard let id else { return }
-            let storageRef = Storage.storage().reference()
             
-              storageRef.child(Constatns.Images.imagePath).child(id).getData(maxSize: 1024 * 1024) { result in
-                switch result {
-                case.success(let imageData):
-                    guard let image = UIImage(data: imageData) else { return }
-                    self.image = image
-                case.failure(let failure):
-                    print(failure.localizedDescription)
-            }
-                
-            }
-            // where are we trying to fetch the image from
         }
     }
     
-    // Fetch single bag
-//    func fetch(parmeterdocID: String) {
-//        let db = Firestore.firestore()
-//        let path = db.collection("Bags").document(parmeterdocID)
-//        path.getDocument(as:Bag.self) {result in
-//            switch result {
-//            case.success(let success):
-//                print(success.name)
-//            case.failure(let failure):
-//                print("",failure.localizedDescription)
-//            }
-//        }
-//
-//
-//    }
     
-    func saveImage(with image: UIImage, to docID: String) { // child is path to image per the docs 
+    // Method singnature
+    func save(parameterBagName: Bag, completion: @escaping (Result<String, FirebaseError>) -> Void) {
+        let ref = Firestore.firestore()
+        do {
+            let documentRef = try ref.collection(Constatns.Bags.bagsCollectionPath).addDocument(from: parameterBagName, completion: { _ in
+            })
+            
+            completion(.success(documentRef.documentID))
+        } catch {
+            print("Oh Shit. Something went wrong", error.localizedDescription)
+            return
+        }
+    } // end of the save
+    
+    func fetchImage(with id: String?) {
+        guard let id else { return }
+        let storageRef = Storage.storage().reference()
+        
+        storageRef.child(Constatns.Images.imagePath).child(id).getData(maxSize: 1024 * 1024) { result in
+            switch result {
+            case.success(let imageData):
+                guard let image = UIImage(data: imageData) else { return }
+                self.image = image
+            case.failure(let failure):
+                print(failure.localizedDescription)
+            }
+            
+        }
+        // where are we trying to fetch the image from
+    }
+    
+    func saveImage(with image: UIImage, to docID: String) { // child is path to image per the docs
         
         // Convert the image to data
         guard let imageData = image.jpegData(compressionQuality: 0.1) else { return }
@@ -108,6 +93,24 @@ class BagDetailViewModel {
             print(imagePath)
         }
     }
-    
-    
-    }
+}
+
+// Fetch single bag
+//    func fetch(parmeterdocID: String) {
+//        let db = Firestore.firestore()
+//        let path = db.collection("Bags").document(parmeterdocID)
+//        path.getDocument(as:Bag.self) {result in
+//            switch result {
+//            case.success(let success):
+//                print(success.name)
+//            case.failure(let failure):
+//                print("",failure.localizedDescription)
+//            }
+//        }
+//
+//
+//    }
+
+
+
+
